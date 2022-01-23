c727c5cf1031eec77dc2811959b9cb0ea3e031d9
unknown <abdallwario96@gmail.com>
2022-01-23 12:17:28 +0300

    uninstalled decouple

--- backend/settings.py
+++ backend/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-from datetime import timedelta
 from decouple import config
+from datetime import timedelta
 import dj_database_url
 
 from pathlib import Path
